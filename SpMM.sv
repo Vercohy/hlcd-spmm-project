f4f6f13760e01b99e67023ab5e39b1c8a66a4767
Verco <2731335150@qq.com>
2024-12-23 18:10:04 +0800

    SpMM 60

--- SpMM.sv
+++ SpMM.sv
@@ -1,3 +1,4 @@
+/* verilator lint_off WIDTHEXPAND */
 `ifndef N
 `define N              16
 `endif
@@ -96,6 +97,7 @@ module PE(
     logic split[`N-1:0];
     logic [`lgN-1:0] out_idx[`N-1:0];
     data_t out_redunit[`N-1:0];
+    int delay_redunit;
     generate
         for(genvar i = 0; i < `N; i++) begin
             mul_ mul_unit(
@@ -154,18 +156,20 @@ module SpMM(
     assign lhs_ready_os = 0;
     assign lhs_ready_wos = 0;
     assign out_ready = 0;
-    // RHS INPUT CTRL
-    data_t rhs_buffer [1:0][`N-1:0][`N-1:0];
-    logic rhs_state;
-    logic [1:0] rhs_idx;
+    int delay_pe;
+    assign delay_pe = `lgN + 1;
+// RHS INPUT CTRL
+    data_t rhs_buffer[`N-1:0][`N-1:0];
+    logic [1:0] rhs_state;
+    logic [`lgN-1:0] rhs_idx;
     //logic rhs_buffer_state [1:0];
-    logic rhs_buffer_state;
+    logic [1:0] rhs_buffer_state;
     logic buffer_choose;
     localparam RHS_EMPTY = 0;
     localparam RHS_BUSY = 1;
     localparam RHS_FILLED = 2;
     always_ff @(posedge clock) begin
-        if (reset) begin
+        if (reset || rhs_idx == `N/4-1) begin
             rhs_idx <= 0;
         end
         else if (rhs_start || rhs_state == RHS_BUSY) begin
@@ -177,36 +181,41 @@ module SpMM(
         if (reset) begin
             rhs_state <= RHS_EMPTY;
         end
-        else if (rhs_start) begin
-            rhs_state <= RHS_BUSY;
-        end
-        else if (rhs_state == RHS_BUSY && rhs_idx == `N/4-1) begin
-            rhs_state <= RHS_FILLED;
-        end
-        else if (rhs_state == RHS_FILLED) begin
-            rhs_state <= RHS_EMPTY;
-        end
+        else case(rhs_state)
+            RHS_EMPTY: begin
+                if (rhs_start) begin
+                    rhs_state <= RHS_BUSY;
+                end
+            end
+            RHS_BUSY: begin
+                if (rhs_idx == `N/4-1 && (rhs_start || rhs_state == RHS_BUSY)) begin
+                    rhs_state <= RHS_FILLED;
+                end
+                else rhs_state <= RHS_BUSY;
+            end
+            RHS_FILLED: begin
+                rhs_state <= RHS_EMPTY;
+            end
+        endcase
     end
+//RHS BUFFER READ
     always_ff @(posedge clock) begin
         if (reset) begin
             //rhs_buffer_state[0] <= RHS_EMPTY;
             //rhs_buffer_state[1] <= RHS_EMPTY;
             //buffer_choose <= 0;
             rhs_buffer_state <= RHS_EMPTY;
-            //for (int i = 0; i < 2; i++) begin
-                for (int j = 0; j < `N; j++) begin
-                    for (int k = 0; k < `N; k++) begin
-                        rhs_buffer[j][k] <= 0;
-                    end
+             for (int j = 0; j < `N; j++) begin
+                for (int k = 0; k < `N; k++) begin
+                    rhs_buffer[j][k].data <= 0;
                 end
-            //end
+            end
         end
         else if (rhs_start || rhs_state == RHS_BUSY) begin
             rhs_buffer_state <= RHS_BUSY;
-            for (int i = 0; i < 3; i++) begin
+            for (int i = 0; i < 4; i++) begin
                 for (int j = 0; j < `N; j++) begin
-                    //rhs_buffer[buffer_choose][i + 4*rhs_idx][j] <= rhs_data[i][j];
-                    rhs_buffer[i + 4*rhs_idx][j] <= rhs_data[i][j];
+                    rhs_buffer[j][i + 4*rhs_idx].data <= rhs_data[i][j].data;
                 end
             end
         end
@@ -216,9 +225,9 @@ module SpMM(
     end
     assign rhs_ready = (rhs_state == RHS_EMPTY);
 
-    //LHS INPUT CTRL
-    logic lhs_state;
-    logic [1:0] lhs_idx;   
+//LHS INPUT CTRL
+    logic [1:0] lhs_state;
+    logic [`lgN-1:0] lhs_idx;   
     localparam LHS_EMPTY = 0; 
     localparam LHS_BUSY = 1;
     localparam LHS_FILLED = 2;
@@ -232,7 +241,7 @@ module SpMM(
         else lhs_idx <= 0;
     end
     always_ff @(posedge clock) begin
-        if (reset || rhs_state == RHS_FILLED) begin //Fix this
+        if (reset || rhs_state == RHS_FILLED) begin
             lhs_state <= LHS_EMPTY;
         end
         else if (lhs_start) begin
@@ -248,20 +257,142 @@ module SpMM(
     end
     assign lhs_ready_ns = (lhs_state == LHS_EMPTY && rhs_buffer_state == RHS_FILLED);
 
-    //RHS Slicing
-    data_t rhs_data_slice[`N-1:0];
-    generate
+// PE CTRL
+    logic pe_lhs_start_signal[1:0];
+    logic pe_lhs_start;
+    always_ff @(posedge clock) begin
+        if (reset) begin
+            pe_lhs_start_signal[1] <= 0;
+        end
+        else begin
+            if ((lhs_idx+1)%4 == 0 && lhs_idx!=`N-1) begin
+                pe_lhs_start_signal[1] <= 1;
+            end
+            else begin
+                pe_lhs_start_signal[1] <= 0;
+            end
+        end
+    end
+    assign pe_lhs_start_signal[0] = lhs_start;
+    assign pe_lhs_start = pe_lhs_start_signal[0] + pe_lhs_start_signal[1];
 
-    endgenerate
-
-    // OUTPUT CTRL
-     
+// OUTPUT CTRL
+    logic [`lgN-1:0] out_state;
     
+    always_ff @(posedge clock) begin
+        if (reset || out_state == delay_pe) begin
+            out_state <= 0;
+        end
+        else if (lhs_idx == `N-1 || out_state != 0) begin
+            out_state <= out_state + 1;
+        end
+    end
+    always_ff @(posedge clock) begin
+        if (reset) begin
+            out_ready <= 0;
+        end
+        else if(out_state == delay_pe) begin
+            out_ready <= 1;
+        end
+        else begin
+            out_ready <= 0;
+        end
+    end
+    //assign out_ready = (out_state == delay_pe);
+
+//OUTPUT BUFFER CTRL
+    data_t out_buffer[`N-1:0][`N-1:0];
+    data_t shift_temp[`N-1:0];
+    data_t out_temp[`N-1:0];
+    logic [`lgN-1:0] out_buf_idx;
+    logic [1:0] out_buffer_state;
+    localparam OUTPUT_BUF_EMPTY = 0;
+    localparam OUTPUT_BUF_BUSY = 1;
+    localparam OUTPUT_BUF_FILLED = 2;
+    always_ff @(posedge clock) begin
+        if (reset) begin
+            out_buffer_state <= OUTPUT_BUF_EMPTY;
+        end
+        else if (lhs_start || (lhs_state == LHS_BUSY)) begin
+            out_buffer_state <= OUTPUT_BUF_BUSY;
+        end
+        else if (out_state == delay_pe-1) begin
+            out_buffer_state <= OUTPUT_BUF_FILLED;
+        end
+    end
+    always_ff @(posedge clock) begin
+        if (reset) begin
+            out_buf_idx <= 0;
+        end
+        else if (out_buf_idx == `N/4) begin
+            out_buffer_state <= OUTPUT_BUF_EMPTY;
+        end
+        else if (out_start || out_buffer_state == OUTPUT_BUF_FILLED) begin
+            out_buf_idx <= out_buf_idx + 1;
+        end
+    end
+// Shift Register
     generate
-        for (genvar i = 0; i < 3; i++) begin
+        for (genvar i = 0; i < `N; i++) begin
             for (genvar j = 0; j < `N; j++) begin
-                assign out_data[i][j] = 0;
+                always_ff @(posedge clock) begin
+                    if (reset) begin
+                        out_buffer[i][j].data <= 0;
+                    end
+                    else if (out_buffer_state == OUTPUT_BUF_BUSY) begin
+                        if (i == `N-1) begin
+                            out_buffer[i][j].data <= out_buffer[0][j].data;
+                        end
+                        else begin
+                            out_buffer[i][j].data <= out_buffer[i+1][j].data;
+                        end
+                    end
+                end
             end
         end
     endgenerate
+//PE
+    int delay;
+    generate
+        for (genvar i = 0; i < `N; i++) begin
+            PE pe_unit(
+                .clock(clock),
+                .reset(reset),
+                .lhs_start(pe_lhs_start),
+                .lhs_ptr(lhs_ptr),
+                .lhs_col(lhs_col),
+                .lhs_data(lhs_data),
+                .rhs(rhs_buffer[i]),
+                .out(out_temp),
+                .delay(delay),
+                .num_el(num_el)
+            );
+            assign out_buffer[`N-1][i].data = out_temp[0].data;
+        end
+    endgenerate
+
+//OUTPUT
+    always_ff @(posedge clock) begin
+        if (reset) begin
+            for (int i = 0; i < 4; i++) begin
+                for (int j = 0; j < `N; j++) begin
+                    out_data[i][j] <= 0;
+                end
+            end
+        end
+        else if (out_state == delay_pe-1 || out_buffer_state == OUTPUT_BUF_FILLED) begin
+            for (int i = 0; i < 4; i++) begin
+                for (int j = 0; j < `N; j++) begin
+                    out_data[i][j].data <= out_buffer[i+out_buf_idx*4][j].data;
+                end
+            end
+        end
+        else begin
+            for (int i = 0; i < 4; i++) begin
+                for (int j = 0; j < `N; j++) begin
+                    out_data[i][j].data <= 0;
+                end
+            end
+        end
+    end
 endmodule
